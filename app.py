10e078b16b7e903f677f7ce22bf965fd4fd119b4
mcarolinag <mcarolinagc@hotmail.com>
2022-11-01 15:09:13 -0700

    First commit
    GPT-3 chatbot

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, session_promptfrom twilio.twiml.messaging_response import messaging_response 
+from bhikkhubot import ask, append_interaction_to_chat_log
+
+app=Flask(__name__)
+
+app.config['SECRET_KEY']='19ca79ro10li21na'
+
+@app.route('/bhikkhubot', method=['POST'])
+
+def bhikkuni():
+    incoming_msg=request.values['Body']
+    chat_log=seesion.get('chat_log')
+    answer=ask(incoming_msg,chat_log)
+    session['chat_log']=append_interaction_to_chat_log(incoming_msg, answer,chat_log)
+    msg-MessagingResponse()
+    msg.message(answer)
+    return str(msg)
+
+if __name__=='__main__':
+    app.run(debug=True)
