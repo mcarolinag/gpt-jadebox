1212f97c998c7a3ad7884085c1a9f576f2a1e408
mcarolinag <mcarolinagc@hotmail.com>
2022-11-02 22:44:14 -0700

    Update app.py
    adding s to methods

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 app.config['SECRET_KEY']='19ca79ro10li21na'
 
-@app.route('/bhikkhubot', method=['POST'])
+@app.route('/bhikkhubot', methods=['POST'])
 
 def bhikkhuni():
     incoming_msg = request.values['Body']
